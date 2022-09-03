aa02ae8441dc3f882d0cd1e98c54caed9258afde
Tim Kinnane <timkinnane@gmail.com>
2022-09-03 21:32:38 +1000

    fix(eslint-config-custom): add standard linting

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,28 @@
+/** @type {import('eslint').Linter.BaseConfig } */
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  env: {
+		es2021: true,
+		node: true,
+		jest: true
+	},
+  extends: [
+    'standard',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'next',
+    'turbo'
+  ],
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
+    '@next/next/no-html-link-for-pages': 'off',
+    'react/jsx-key': 'off',
   },
+  parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module'
+	},
+	plugins: [
+		'@typescript-eslint'
+	]
 };
